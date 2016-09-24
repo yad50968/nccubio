423633f55ee6ad9c30a4b046443851c440e809a2
Liu Tzu Yuan <liutzuyuan@LiudeMacBook-Pro.local>
2016-09-24 21:27:28 +0800

    fix bug

--- hw1/hw1_105753036.R
+++ hw1/hw1_105753036.R
@@ -1,12 +1,16 @@
 # read PAM1 from data
-pam1<-read.table("./pam1.txt")
+pam1<-as.matrix(read.table("./pam1.txt"))
 
-# check PAM1 data
-dim(pam1)
-str(pam1)
+pam1 <- pam1/10000  
+#avoid number too large
 
-# construct PAM250 from PAM1
-pam250 <- pam1^250
+
+pam250 <- pam1
+for (x in c(1:250)) pam250 <- pam250 %*% pam1
+
+
+pam250 <- pam250 * 100
 
 # output PAM250 as a file
 write.table(pam250,"./pam250.txt")
+
